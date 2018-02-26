e7edafe66789cb35d611511d6bc92c3e274fb29d
Administrator <lijiarong@sensetime.com>
2018-02-26 17:16:55 +0800

    config

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+
+__author__='Search Lee'
+
+'''
+Default configrations.
+'''
+
+configs={
+    'db':{
+        'host':'127.0.0.0',
+        'port':3306,
+        'user':'webapp',
+        'password':'webapp',
+        'database':'awesome'
+    },
+    'session':{
+        'secret':'AwEsOmE'
+    }
+}
